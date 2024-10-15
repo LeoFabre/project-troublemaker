0ab74e7c5f0d41081036e0c7ae3ca73c1cd85a13
Leo Fabre <fabre.leo@gmail.com>
2024-10-15 02:17:15 +0200

    EQ+Xover prototype code

--- midi_controller/src/main.cpp
+++ midi_controller/src/main.cpp
@@ -3,37 +3,55 @@
 
 USBMIDI_Interface midi_interface;
 
-//Analog multiplexer on Arduino analog pin A0, enable pin on pin 6
+//Analog multiplexer on Arduino analog pin A10, enable pin on pin 6
 CD74HC4067 mux1 = {
-    A0,
+    A10,
     {2, 3, 4, 5},
     6,
 };
 
-//Analog multiplexer on Arduino analog pin A0, enable pin on pin 7
+//Analog multiplexer on Arduino analog pin A10, enable pin on pin 7
 CD74HC4067 mux2 = {
-    A0,
+    A10,
     {2, 3, 4, 5},
     7,
 };
 
-CCPotentiometer pot1 = {mux1.pin(0), {MIDI_CC::General_Purpose_Controller_1, Channel_1}};
-CCPotentiometer pot2 = {mux1.pin(1), {MIDI_CC::General_Purpose_Controller_1, Channel_2}};
-CCPotentiometer pot3 = {mux1.pin(2), {MIDI_CC::General_Purpose_Controller_1, Channel_3}};
-CCPotentiometer pot4 = {mux1.pin(3), {MIDI_CC::General_Purpose_Controller_1, Channel_4}};
-CCPotentiometer pot5 = {mux1.pin(4), {MIDI_CC::General_Purpose_Controller_1, Channel_5}};
-CCPotentiometer pot6 = {mux1.pin(5), {MIDI_CC::General_Purpose_Controller_1, Channel_6}};
-CCPotentiometer pot7 = {mux1.pin(6), {MIDI_CC::General_Purpose_Controller_1, Channel_7}};
-CCPotentiometer pot8 = {mux1.pin(7), {MIDI_CC::General_Purpose_Controller_1, Channel_8}};
+CCButton XHighSwitch = {mux2.pin(0), {MIDI_CC::General_Purpose_Controller_5, Channel_1}};
+CCPotentiometer XHighAtk = {mux2.pin(1), {MIDI_CC::General_Purpose_Controller_5, Channel_2}};
+CCPotentiometer XHighTresh = {mux2.pin(2), {MIDI_CC::General_Purpose_Controller_5, Channel_3}};
+CCPotentiometer XHighRel = {mux2.pin(3), {MIDI_CC::General_Purpose_Controller_5, Channel_4}};
+CCPotentiometer XHighGain = {mux2.pin(4), {MIDI_CC::General_Purpose_Controller_5, Channel_5}};
 
-CCPotentiometer pot9 = {mux2.pin(0), {MIDI_CC::General_Purpose_Controller_1, Channel_9}};
-CCPotentiometer pot10 = {mux2.pin(1), {MIDI_CC::General_Purpose_Controller_1, Channel_10}};
-CCPotentiometer pot11 = {mux2.pin(2), {MIDI_CC::General_Purpose_Controller_1, Channel_11}};
-CCPotentiometer pot12 = {mux2.pin(3), {MIDI_CC::General_Purpose_Controller_1, Channel_12}};
-CCPotentiometer pot13 = {mux2.pin(4), {MIDI_CC::General_Purpose_Controller_1, Channel_13}};
-CCPotentiometer pot14 = {mux2.pin(5), {MIDI_CC::General_Purpose_Controller_1, Channel_14}};
-CCPotentiometer pot15 = {mux2.pin(6), {MIDI_CC::General_Purpose_Controller_1, Channel_15}};
-CCPotentiometer pot16 = {mux2.pin(7), {MIDI_CC::General_Purpose_Controller_1, Channel_16}};
+CCButton XMidSwitch = {mux2.pin(5), {MIDI_CC::General_Purpose_Controller_4, Channel_1}};
+CCPotentiometer XMidAtk = {mux2.pin(6), {MIDI_CC::General_Purpose_Controller_4, Channel_2}};
+CCPotentiometer XMidTresh = {mux2.pin(7), {MIDI_CC::General_Purpose_Controller_4, Channel_3}};
+CCPotentiometer XMidRel = {mux2.pin(8), {MIDI_CC::General_Purpose_Controller_4, Channel_2}};
+CCPotentiometer XMidGain = {mux2.pin(9), {MIDI_CC::General_Purpose_Controller_4, Channel_5}};
+
+CCButton XBassSwitch = {mux2.pin(10), {MIDI_CC::General_Purpose_Controller_3, Channel_1}};
+CCPotentiometer XBassAtk = {mux2.pin(11), {MIDI_CC::General_Purpose_Controller_3, Channel_2}};
+CCPotentiometer XBassTresh = {mux2.pin(12), {MIDI_CC::General_Purpose_Controller_3, Channel_3}};
+CCPotentiometer XBassRel = {mux2.pin(13), {MIDI_CC::General_Purpose_Controller_3, Channel_4}};
+CCPotentiometer XBassGain = {mux2.pin(14), {MIDI_CC::General_Purpose_Controller_3, Channel_5}};
+
+CCButton XSubSwitch = {mux1.pin(0), {MIDI_CC::General_Purpose_Controller_2, Channel_1}};
+CCPotentiometer XSubAtk = {mux1.pin(1), {MIDI_CC::General_Purpose_Controller_2, Channel_2}};
+CCPotentiometer XSubTresh = {mux1.pin(2), {MIDI_CC::General_Purpose_Controller_2, Channel_3}};
+CCPotentiometer XSubRel = {mux1.pin(3), {MIDI_CC::General_Purpose_Controller_2, Channel_4}};
+CCPotentiometer XSubGain = {mux1.pin(4), {MIDI_CC::General_Purpose_Controller_2, Channel_5}};
+
+CCButton Eq2Switch = {mux1.pin(5), {MIDI_CC::General_Purpose_Controller_1, Channel_5}};
+CCPotentiometer Eq2Freq = {mux1.pin(6), {MIDI_CC::General_Purpose_Controller_1, Channel_6}};
+CCPotentiometer Eq2Gain = {mux1.pin(7), {MIDI_CC::General_Purpose_Controller_1, Channel_7}};
+CCPotentiometer Eq2Q = {mux1.pin(8), {MIDI_CC::General_Purpose_Controller_1, Channel_8}};
+
+CCPotentiometer InputGain = {mux1.pin(9), {MIDI_CC::General_Purpose_Controller_5, Channel_1}};
+
+CCButton Eq1Switch = {mux1.pin(10), {MIDI_CC::General_Purpose_Controller_1, Channel_1}};
+CCPotentiometer Eq1Freq = {mux1.pin(11), {MIDI_CC::General_Purpose_Controller_1, Channel_2}};
+CCPotentiometer Eq1Gain = {mux1.pin(12), {MIDI_CC::General_Purpose_Controller_1, Channel_3}};
+CCPotentiometer Eq1Q = {mux1.pin(13), {MIDI_CC::General_Purpose_Controller_1, Channel_4}};
 
 void setup()
 {
