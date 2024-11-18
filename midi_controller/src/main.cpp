e004f3edd895232e5a61ecc2e9368424ea83c517
Leo Fabre <fabre.leo@gmail.com>
2024-11-18 22:30:50 +0100

    Grouped controls channels by plugin, now using 0 to N

--- midi_controller/src/main.cpp
+++ midi_controller/src/main.cpp
@@ -69,113 +69,114 @@ NoteButtons<12> samplerButtons(
     {59, Channel_1}, // Adresse MIDI de base
     {1} // Incrément pour chaque bouton
 );
-CCPotentiometer SamplerSpeed = {mux1.pin(0), {MIDI_CC::General_Purpose_Controller_1, Channel_13}};
-CCPotentiometer SamplerReverb = {mux1.pin(1), {MIDI_CC::General_Purpose_Controller_1, Channel_14}};
-CCPotentiometer SamplerDelay = {mux1.pin(2), {MIDI_CC::General_Purpose_Controller_1, Channel_15}};
-CCPotentiometer SamplerGrain = {mux1.pin(3), {MIDI_CC::General_Purpose_Controller_1, Channel_16}};
-CCPotentiometer SamplerLevel = {mux1.pin(4), {MIDI_CC::General_Purpose_Controller_2, Channel_1}};
+CCPotentiometer SamplerSpeed = {mux1.pin(0), {0, Channel_1}};
+CCPotentiometer SamplerReverb = {mux1.pin(1), {1, Channel_1}};
+CCPotentiometer SamplerDelay = {mux1.pin(2), {2, Channel_1}};
+CCPotentiometer SamplerGrain = {mux1.pin(3), {3, Channel_1}};
+CCPotentiometer SamplerLevel = {mux1.pin(4), {4, Channel_1}};
 
 //EQ controls
-CCButton Eq1Switch = {A1, {MIDI_CC::General_Purpose_Controller_2, Channel_1}};
-CCPotentiometer Eq1Freq = {mux1.pin(5), {MIDI_CC::General_Purpose_Controller_2, Channel_2}};
-CCPotentiometer Eq1Gain = {mux1.pin(6), {MIDI_CC::General_Purpose_Controller_2, Channel_3}};
-CCPotentiometer Eq1Q = {mux1.pin(7), {MIDI_CC::General_Purpose_Controller_2, Channel_4}};
+CCButton Eq1Switch = {A1, {0, Channel_2}};
+CCPotentiometer Eq1Freq = {mux1.pin(5), {1, Channel_2}};
+CCPotentiometer Eq1Gain = {mux1.pin(6), {2, Channel_2}};
+CCPotentiometer Eq1Q = {mux1.pin(7), {3, Channel_2}};
 
-CCButton Eq2Switch = {A2, {MIDI_CC::General_Purpose_Controller_2, Channel_5}};
-CCPotentiometer Eq2Freq = {mux1.pin(8), {MIDI_CC::General_Purpose_Controller_2, Channel_6}};
-CCPotentiometer Eq2Gain = {mux1.pin(9), {MIDI_CC::General_Purpose_Controller_2, Channel_7}};
-CCPotentiometer Eq2Q = {mux1.pin(10), {MIDI_CC::General_Purpose_Controller_2, Channel_8}};
+CCButton Eq2Switch = {A2, {4, Channel_2}};
+CCPotentiometer Eq2Freq = {mux1.pin(8), {5, Channel_2}};
+CCPotentiometer Eq2Gain = {mux1.pin(9), {6, Channel_2}};
+CCPotentiometer Eq2Q = {mux1.pin(10), {7, Channel_2}};
 
 //Crossover controls
-CCButton XSubSwitch = {A3, {MIDI_CC::General_Purpose_Controller_2, Channel_9}};
-CCPotentiometer XSubAtk = {mux1.pin(11), {MIDI_CC::General_Purpose_Controller_2, Channel_10}};
-CCPotentiometer XSubRel = {mux1.pin(12), {MIDI_CC::General_Purpose_Controller_2, Channel_12}};
-CCPotentiometer XSubTresh = {mux1.pin(13), {MIDI_CC::General_Purpose_Controller_2, Channel_11}};
-CCPotentiometer XSubGain = {mux1.pin(14), {MIDI_CC::General_Purpose_Controller_2, Channel_13}};
+CCButton XSubSwitch = {A3, {0, Channel_3}};
+CCPotentiometer XSubAtk = {mux1.pin(11), {1, Channel_3}};
+CCPotentiometer XSubRel = {mux1.pin(12), {2, Channel_3}};
+CCPotentiometer XSubTresh = {mux1.pin(13), {3, Channel_3}};
+CCPotentiometer XSubGain = {mux1.pin(14), {4, Channel_3}};
 
-CCButton XBassSwitch = {A4, {MIDI_CC::General_Purpose_Controller_2, Channel_14}};
-CCPotentiometer XBassAtk = {mux1.pin(15), {MIDI_CC::General_Purpose_Controller_2, Channel_15}};
-CCPotentiometer XBassRel = {mux2.pin(0), {MIDI_CC::General_Purpose_Controller_2, Channel_16}};
-CCPotentiometer XBassTresh = {mux2.pin(1), {MIDI_CC::General_Purpose_Controller_3, Channel_1}};
-CCPotentiometer XBassGain = {mux2.pin(2), {MIDI_CC::General_Purpose_Controller_3, Channel_2}};
+CCButton XBassSwitch = {A4, {5, Channel_3}};
+CCPotentiometer XBassAtk = {mux1.pin(15), {6, Channel_3}};
+CCPotentiometer XBassRel = {mux2.pin(0), {7, Channel_3}};
+CCPotentiometer XBassTresh = {mux2.pin(1), {8, Channel_3}};
+CCPotentiometer XBassGain = {mux2.pin(2), {9, Channel_3}};
 
-CCButton XMidSwitch = {A5, {MIDI_CC::General_Purpose_Controller_3, Channel_3}};
-CCPotentiometer XMidAtk = {mux2.pin(3), {MIDI_CC::General_Purpose_Controller_3, Channel_4}};
-CCPotentiometer XMidRel = {mux2.pin(4), {MIDI_CC::General_Purpose_Controller_3, Channel_5}};
-CCPotentiometer XMidTresh = {mux2.pin(5), {MIDI_CC::General_Purpose_Controller_3, Channel_6}};
-CCPotentiometer XMidGain = {mux2.pin(6), {MIDI_CC::General_Purpose_Controller_3, Channel_7}};
+CCButton XMidSwitch = {A5, {10, Channel_3}};
+CCPotentiometer XMidAtk = {mux2.pin(3), {11, Channel_3}};
+CCPotentiometer XMidRel = {mux2.pin(4), {12, Channel_3}};
+CCPotentiometer XMidTresh = {mux2.pin(5), {13, Channel_3}};
+CCPotentiometer XMidGain = {mux2.pin(6), {14, Channel_3}};
 
-CCButton XHighSwitch = {A6, {MIDI_CC::General_Purpose_Controller_3, Channel_8}};
-CCPotentiometer XHighAtk = {mux2.pin(7), {MIDI_CC::General_Purpose_Controller_3, Channel_9}};
-CCPotentiometer XHighRel = {mux2.pin(8), {MIDI_CC::General_Purpose_Controller_3, Channel_10}};
-CCPotentiometer XHighTresh = {mux2.pin(9), {MIDI_CC::General_Purpose_Controller_3, Channel_11}};
-CCPotentiometer XHighGain = {mux2.pin(10), {MIDI_CC::General_Purpose_Controller_3, Channel_12}};
+CCButton XHighSwitch = {A6, {15, Channel_3}};
+CCPotentiometer XHighAtk = {mux2.pin(7), {16, Channel_3}};
+CCPotentiometer XHighRel = {mux2.pin(8), {17, Channel_3}};
+CCPotentiometer XHighTresh = {mux2.pin(9), {18, Channel_3}};
+CCPotentiometer XHighGain = {mux2.pin(10), {19, Channel_3}};
 
 //Delay controls
-CCPotentiometer DelayLFORate = {mux2.pin(11), {MIDI_CC::General_Purpose_Controller_3, Channel_13}};
-CCPotentiometer DelayLFODepth = {mux2.pin(12), {MIDI_CC::General_Purpose_Controller_3, Channel_14}};
-CCPotentiometer DelayTime = {mux2.pin(13), {MIDI_CC::General_Purpose_Controller_3, Channel_15}};
-CCPotentiometer DelayFeedback = {mux2.pin(14), {MIDI_CC::General_Purpose_Controller_3, Channel_16}};
-CCPotentiometer DelaySampleRate = {mux2.pin(15), {MIDI_CC::General_Purpose_Controller_4, Channel_1}};
-CCPotentiometer DelayLowCutResonance = {mux3.pin(0), {MIDI_CC::General_Purpose_Controller_4, Channel_2}};
-CCPotentiometer DelayLowCutFreq = {mux3.pin(1), {MIDI_CC::General_Purpose_Controller_4, Channel_3}};
-CCPotentiometer DelayHighCutResonance = {mux3.pin(2), {MIDI_CC::General_Purpose_Controller_4, Channel_4}};
-CCPotentiometer DelayHighCutFreq = {mux3.pin(3), {MIDI_CC::General_Purpose_Controller_4, Channel_5}};
-CCPotentiometer DelayStereoWidth = {mux3.pin(4), {MIDI_CC::General_Purpose_Controller_4, Channel_6}};
-CCPotentiometer DelayNoise = {mux3.pin(5), {MIDI_CC::General_Purpose_Controller_4, Channel_7}};
-CCPotentiometer DelayLevel = {mux3.pin(6), {MIDI_CC::General_Purpose_Controller_4, Channel_8}};
-CCButtonLatched DelayTapTempo = {A7, {MIDI_CC::General_Purpose_Controller_4, Channel_9}};
-CCButton DelayType = {A8, {MIDI_CC::General_Purpose_Controller_4, Channel_10}};
+CCPotentiometer DelayLFORate = {mux2.pin(11), {0, Channel_4}};
+CCPotentiometer DelayLFODepth = {mux2.pin(12), {1, Channel_4}};
+CCPotentiometer DelayTime = {mux2.pin(13), {2, Channel_4}};
+CCPotentiometer DelayFeedback = {mux2.pin(14), {3, Channel_4}};
+CCPotentiometer DelaySampleRate = {mux2.pin(15), {4, Channel_4}};
+CCPotentiometer DelayLowCutResonance = {mux3.pin(0), {5, Channel_4}};
+CCPotentiometer DelayLowCutFreq = {mux3.pin(1), {6, Channel_4}};
+CCPotentiometer DelayHighCutResonance = {mux3.pin(2), {7, Channel_4}};
+CCPotentiometer DelayHighCutFreq = {mux3.pin(3), {8, Channel_4}};
+CCPotentiometer DelayStereoWidth = {mux3.pin(4), {9, Channel_4}};
+CCPotentiometer DelayNoise = {mux3.pin(5), {10, Channel_4}};
+CCPotentiometer DelayLevel = {mux3.pin(6), {11, Channel_4}};
+CCButtonLatched DelayTapTempo = {A7, {12, Channel_4}};
+CCButton DelayType = {A8, {13, Channel_4}};
 
 //Grain controls
-CCPotentiometer GrainPitch = {mux3.pin(7), {MIDI_CC::General_Purpose_Controller_4, Channel_11}};
-CCPotentiometer GrainFine = {mux3.pin(8), {MIDI_CC::General_Purpose_Controller_4, Channel_12}};
-CCPotentiometer GrainSize = {mux3.pin(9), {MIDI_CC::General_Purpose_Controller_4, Channel_13}};
-CCPotentiometer GrainTexture = {mux3.pin(10), {MIDI_CC::General_Purpose_Controller_4, Channel_14}};
-CCPotentiometer GrainStretch = {mux3.pin(11), {MIDI_CC::General_Purpose_Controller_4, Channel_15}};
-CCPotentiometer GrainFeedback = {mux3.pin(12), {MIDI_CC::General_Purpose_Controller_4, Channel_16}};
-CCPotentiometer GrainShimmer = {mux3.pin(13), {MIDI_CC::General_Purpose_Controller_5, Channel_1}};
-CCPotentiometer GrainHighCut = {mux3.pin(14), {MIDI_CC::General_Purpose_Controller_5, Channel_2}};
-CCPotentiometer GrainLevel = {mux3.pin(15), {MIDI_CC::General_Purpose_Controller_5, Channel_3}};
-CCButton GrainType = {A14, {MIDI_CC::General_Purpose_Controller_5, Channel_4}};
-CCButton GrainPlayback = {A15, {MIDI_CC::General_Purpose_Controller_5, Channel_5}};
+CCPotentiometer GrainPitch = {mux3.pin(7), {0, Channel_5}};
+CCPotentiometer GrainFine = {mux3.pin(8), {1, Channel_5}};
+CCPotentiometer GrainSize = {mux3.pin(9), {2, Channel_5}};
+CCPotentiometer GrainTexture = {mux3.pin(10), {3, Channel_5}};
+CCPotentiometer GrainStretch = {mux3.pin(11), {4, Channel_5}};
+CCPotentiometer GrainFeedback = {mux3.pin(12), {5, Channel_5}};
+CCPotentiometer GrainShimmer = {mux3.pin(13), {6, Channel_5}};
+CCPotentiometer GrainHighCut = {mux3.pin(14), {7, Channel_5}};
+CCPotentiometer GrainLevel = {mux3.pin(15), {8, Channel_5}};
+CCButton GrainType = {A14, {9, Channel_5}};
+CCButton GrainPlayback = {A15, {10, Channel_5}};
 
 //Inputs controls
-CCPotentiometer InputAGain = {mux4.pin(0), {MIDI_CC::General_Purpose_Controller_5, Channel_6}};
-CCPotentiometer InputADelaySend = {mux4.pin(2), {MIDI_CC::General_Purpose_Controller_5, Channel_7}};
-CCPotentiometer InputAGrainSend = {mux4.pin(4), {MIDI_CC::General_Purpose_Controller_5, Channel_8}};
-CCPotentiometer InputASubRoarSend = {mux4.pin(6), {MIDI_CC::General_Purpose_Controller_5, Channel_9}};
-CCPotentiometer InputAReverbSend = {mux4.pin(8), {MIDI_CC::General_Purpose_Controller_5, Channel_10}};
-CCPotentiometer InputAAuxSend = {mux4.pin(10), {MIDI_CC::General_Purpose_Controller_5, Channel_11}};
+CCPotentiometer InputAGain = {mux4.pin(0), {0, Channel_6}};
+CCPotentiometer InputADelaySend = {mux4.pin(2), {1, Channel_6}};
+CCPotentiometer InputAGrainSend = {mux4.pin(4), {2, Channel_6}};
+CCPotentiometer InputASubRoarSend = {mux4.pin(6), {3, Channel_6}};
+CCPotentiometer InputAReverbSend = {mux4.pin(8), {4, Channel_6}};
+CCPotentiometer InputAAuxSend = {mux4.pin(10), {5, Channel_6}};
 
-CCPotentiometer InputBGain = {mux4.pin(1), {MIDI_CC::General_Purpose_Controller_5, Channel_12}};
-CCPotentiometer InputBDelaySend = {mux4.pin(3), {MIDI_CC::General_Purpose_Controller_5, Channel_13}};
-CCPotentiometer InputBGrainSend = {mux4.pin(5), {MIDI_CC::General_Purpose_Controller_5, Channel_14}};
-CCPotentiometer InputBSubRoarSend = {mux4.pin(7), {MIDI_CC::General_Purpose_Controller_5, Channel_15}};
-CCPotentiometer InputBReverbSend = {mux4.pin(9), {MIDI_CC::General_Purpose_Controller_5, Channel_16}};
+//no input b for now, stereo input only
+// CCPotentiometer InputBGain = {mux4.pin(1), {6, Channel_12}};
+// CCPotentiometer InputBDelaySend = {mux4.pin(3), {7, Channel_13}};
+// CCPotentiometer InputBGrainSend = {mux4.pin(5), {8, Channel_14}};
+// CCPotentiometer InputBSubRoarSend = {mux4.pin(7), {9, Channel_15}};
+// CCPotentiometer InputBReverbSend = {mux4.pin(9), {10, Channel_16}};
 
 //Sub roar controls
-CCPotentiometer SubRoarInGain = {mux4.pin(11), {MIDI_CC::General_Purpose_Controller_6, Channel_1}};
-CCPotentiometer SubRoarLowPassFreq = {mux4.pin(12), {MIDI_CC::General_Purpose_Controller_6, Channel_2}};
-CCPotentiometer SubRoarFold = {mux4.pin(13), {MIDI_CC::General_Purpose_Controller_6, Channel_3}};
-CCPotentiometer SubRoarEmphasisFreq = {mux5.pin(0), {MIDI_CC::General_Purpose_Controller_6, Channel_4}};
-CCPotentiometer SubRoarEmphasisGain = {mux5.pin(1), {MIDI_CC::General_Purpose_Controller_6, Channel_5}};
-CCPotentiometer SubRoarReverbMix = {mux5.pin(2), {MIDI_CC::General_Purpose_Controller_6, Channel_6}};
-CCPotentiometer SubRoarReverbRoomSize = {mux5.pin(3), {MIDI_CC::General_Purpose_Controller_6, Channel_7}};
-CCPotentiometer SubRoarReverbDecay = {mux5.pin(4), {MIDI_CC::General_Purpose_Controller_6, Channel_8}};
-CCPotentiometer SubRoarLevel = {mux5.pin(5), {MIDI_CC::General_Purpose_Controller_6, Channel_9}};
+CCPotentiometer SubRoarInGain = {mux4.pin(11), {0, Channel_7}};
+CCPotentiometer SubRoarLowPassFreq = {mux4.pin(12), {1, Channel_7}};
+CCPotentiometer SubRoarFold = {mux4.pin(13), {2, Channel_7}};
+CCPotentiometer SubRoarEmphasisFreq = {mux5.pin(0), {3, Channel_7}};
+CCPotentiometer SubRoarEmphasisGain = {mux5.pin(1), {4, Channel_7}};
+CCPotentiometer SubRoarReverbMix = {mux5.pin(2), {5, Channel_7}};
+CCPotentiometer SubRoarReverbRoomSize = {mux5.pin(3), {6, Channel_7}};
+CCPotentiometer SubRoarReverbDecay = {mux5.pin(4), {7, Channel_7}};
+CCPotentiometer SubRoarLevel = {mux5.pin(5), {8, Channel_7}};
 
 //Reverb controls
-CCPotentiometer ReverbLowCutFreq = {mux5.pin(6), {MIDI_CC::General_Purpose_Controller_6, Channel_10}};
-CCPotentiometer ReverbHighCutFreq = {mux5.pin(7), {MIDI_CC::General_Purpose_Controller_6, Channel_11}};
-CCPotentiometer ReverbDecay = {mux5.pin(8), {MIDI_CC::General_Purpose_Controller_6, Channel_12}};
-CCPotentiometer ReverbPreDelay = {mux5.pin(9), {MIDI_CC::General_Purpose_Controller_6, Channel_13}};
-CCPotentiometer ReverbRoomSize = {mux5.pin(10), {MIDI_CC::General_Purpose_Controller_6, Channel_14}};
-CCPotentiometer ReverbBellResonance = {mux5.pin(11), {MIDI_CC::General_Purpose_Controller_6, Channel_15}};
-CCPotentiometer ReverbBellFrequency = {mux5.pin(12), {MIDI_CC::General_Purpose_Controller_6, Channel_16}};
-CCPotentiometer ReverbLFODepth = {mux5.pin(13), {MIDI_CC::General_Purpose_Controller_7, Channel_1}};
-CCPotentiometer ReverbLFORate = {mux5.pin(14), {MIDI_CC::General_Purpose_Controller_7, Channel_2}};
-CCPotentiometer ReverbLevel = {mux5.pin(15), {MIDI_CC::General_Purpose_Controller_7, Channel_3}};
+CCPotentiometer ReverbLowCutFreq = {mux5.pin(6), {0, Channel_8}};
+CCPotentiometer ReverbHighCutFreq = {mux5.pin(7), {1, Channel_8}};
+CCPotentiometer ReverbDecay = {mux5.pin(8), {2, Channel_8}};
+CCPotentiometer ReverbPreDelay = {mux5.pin(9), {3, Channel_8}};
+CCPotentiometer ReverbRoomSize = {mux5.pin(10), {4, Channel_8}};
+CCPotentiometer ReverbBellResonance = {mux5.pin(11), {5, Channel_8}};
+CCPotentiometer ReverbBellFrequency = {mux5.pin(12), {6, Channel_8}};
+CCPotentiometer ReverbLFODepth = {mux5.pin(13), {7, Channel_8}};
+CCPotentiometer ReverbLFORate = {mux5.pin(14), {8, Channel_8}};
+CCPotentiometer ReverbLevel = {mux5.pin(15), {9, Channel_8}};
 
 void setup()
 {
